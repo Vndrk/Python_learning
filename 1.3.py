ce161a35d9903ad74cf25dcc20b93649e55b7637
Greg Avsyannikov <vndrk@tut.by>
2022-10-12 00:39:11 +0300

    00:39

--- 1.3.py
+++
@@ -1,8 +0,0 @@
-# Узнайте у пользователя число n. Найдите сумму чисел n + nn + nnn. Например, пользователь
-# ввёл число 3. Считаем 3 + 33 + 333 = 369
-number = input('введите число n:')
-element_1 = number
-element_2 = number + number
-element_3 = number + number + number
-sum_elements = int(element_1) + int(element_2) + int(element_3)
-print(sum_elements)
